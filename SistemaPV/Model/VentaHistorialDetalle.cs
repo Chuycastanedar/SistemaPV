54acaafe61ceb78341fbbd5705c6d1fa4b1c087c
Chuycastanedar <chuycastaneda_@outlook.com>
2025-11-11 22:12:39 -0600

    Implementación completa de cajero al patrón MVVM...

---
+++ SistemaPV/Model/VentaHistorialDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPV.Model
+{
+    public class VentaHistorialDetalle
+    {
+        public string NOMBRE_PRODUCTO { get; set; }
+        public int CANTIDAD { get; set; }
+        public decimal PRECIO_UNITARIO { get; set; }
+        public decimal SUBTOTAL => CANTIDAD * PRECIO_UNITARIO;
+    }
+}
